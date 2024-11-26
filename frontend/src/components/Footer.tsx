afc35dcc6c2b1ba601f802bfbd0b424eebae7f00
Aryan Salaria <arayn.32@gmail.com>
2024-11-27 00:30:46 +0530

    Initial Commit

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import "../styles/index.scss";
+
+const Footer: React.FC = () => {
+
+  return (
+    <footer>
+      <span>© 2023 Copyright</span>
+    </footer>
+  );
+};
+
+export default Footer;
